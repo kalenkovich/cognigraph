b60753fe344f50072a02991c9c9ae8363e51dae3
Evgenii <e.kalenkovich@gmail.com>
2017-11-13 19:30:09 +0300

    Added run and init methods and self-destruction for InputNode

--- cognigraph/node.py
+++ cognigraph/node.py
@@ -1,20 +1,49 @@
+import time
+
 class Node(object):
     """ Any processing step (including getting and outputing data) is an instance of this class """
 
     def __init__(self):
-        pass
+        pass  # TODO: implement
+
+    def init(self):
+        pass  # TODO: implement
+
+    def run(self):
+        pass  # TODO: implement
 
 
 class InputNode(Node):
     """ Objects of this class read data from a source """
 
-    def __init__(self):
+    def __init__(self, seconds_to_live=None):
         super().__init__()
+        self._birthtime = None  # TODO: remove this self-destruction nonsense
+        self._seconds_to_live = seconds_to_live
+        self._is_alive = True
+        # TODO: implement reasonably or leave empty
+
+    @property
+    def is_alive(self):
+        current_time_in_s = time.time()
+        if current_time_in_s > self._birthtime + self._seconds_to_live:
+            self._is_alive = False
+        return self._is_alive
+
+    def init(self):
+        super().init()
+        self._birthtime = time.time()
+
+    def run(self):
+        super.run()
+
+
+
 
 
 class ProcessorNode(Node):
-    pass
+    pass  # TODO: implement
 
 
 class OutputNode(Node):
-    pass
+    pass  # TODO: implement
