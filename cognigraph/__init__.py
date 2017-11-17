960c51db1f79cacd736e1a84458057719bc95916
Evgenii <e.kalenkovich@gmail.com>
2017-11-18 00:04:56 +0300

    Added chunk conversion between LSL and numpy plus an error fix
    Type (kinda) in convert_numpy_format_to_lsl

--- cognigraph/__init__.py
+++ cognigraph/__init__.py
@@ -1,6 +1,8 @@
 __all__ = ["node", "pipeline"]
 
-from .node import Node, InputNode, OutputNode, ProcessorNode
-from .pipeline import Pipeline
+# from .node import Node, SourceNode, OutputNode, ProcessorNode
+# from .pipeline import Pipeline
 
 # TODO: I wish this was an empty file
+
+TIME_DIMENSION_ID = 1
--- cognigraph/helpers/lsl.py
+++ cognigraph/helpers/lsl.py
@@ -1,7 +1,12 @@
+import uuid
+
 import pylsl as lsl
 import numpy as np
 from pylsl.pylsl import fmt2string, string2fmt
-import uuid
+
+from .. import TIME_DIMENSION_ID
+
+LSL_TIME_DIMENSION_ID = 0
 
 
 def convert_lsl_format_to_numpy(lsl_channel_format: int):
@@ -9,10 +14,10 @@ def convert_lsl_format_to_numpy(lsl_channel_format: int):
 
 
 def convert_numpy_format_to_lsl(numpy_channel_format: np.dtype):
-    return string2fmt[str(np.dtype)]
+    return string2fmt[str(numpy_channel_format)]
 
 
-def create_lsl_outlet(name, type, frequency, channel_format, channel_labels):
+def create_lsl_outlet(name, frequency, channel_format, channel_labels, type=''):
     # Create StreamInfo
     channel_cnt = len(channel_labels)
     source_id = str(uuid.uuid4())
@@ -28,3 +33,28 @@ def create_lsl_outlet(name, type, frequency, channel_format, channel_labels):
 
     # Create outlet
     return lsl.StreamOutlet(info)
+
+
+def _transpose_if_need_be(ndarray: np.ndarray):
+    """ Just a helper function for the next two conversion functions """
+    if LSL_TIME_DIMENSION_ID != TIME_DIMENSION_ID:
+        return ndarray.T
+    else:
+        return ndarray
+
+
+def convert_lsl_chunk_to_numpy_array(lsl_chunk, dtype=None):
+    """
+    An LSL chunk is a list of lists. Converting it to a numpy array and vice versa is obviously a trivial matter not
+    worthy of a designated function. What *does* need taking care of, is whether we should transpose the numpy array or
+    not. In LSL time is the first dimension. We might or might not adhere to this convention, which is reflected in the
+    TIME_DIMENSION_ID constant from the base package.
+    """
+    ndarray = np.array(lsl_chunk, dtype=dtype)
+    return _transpose_if_need_be(ndarray)
+
+
+def convert_numpy_array_to_lsl_chunk(ndarray):
+    ndarray = _transpose_if_need_be(ndarray)
+    return ndarray.tolist()
+convert_numpy_array_to_lsl_chunk.__doc__ = convert_lsl_chunk_to_numpy_array.__doc__
