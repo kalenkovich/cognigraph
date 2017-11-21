1db192b47179f11ba9ebf103972243eca59ef945
Evgenii <e.kalenkovich@gmail.com>
2017-11-21 10:44:24 +0300

    Added functionality to Pipeline

--- cognigraph/pipeline.py
+++ cognigraph/pipeline.py
@@ -1,5 +1,8 @@
-from cognigraph.nodes.node import SourceNode, ProcessorNode, OutputNode
+from typing import List
+
+from cognigraph.nodes.node import Node, SourceNode, ProcessorNode, OutputNode
 from .helpers.decorators import accepts
+from .helpers.misc import class_name_of
 
 
 class Pipeline(object):
@@ -11,44 +14,93 @@ class Pipeline(object):
 
     Sample usage:
 
-    p = Pipeline();
-    p.input = lsl_input_stream
-    p.addProcessor(linear_filter_1_40)
-    p.addOutput(brainvision_output);
-    p.run()
+    pipeline = Pipeline()
+    pipeline.source = sources.LSLStreamSource(stream_name='cognigraph-mock-stream')
+    linear_filter = processors.LinearFilter(lower_cutoff=0.1, upper_cutoff=40)
+    pipeline.add_processor(linear_filter)
+    pipeline.add_processor(processors.InverseModel(method='MNE'))
+    pipeline.add_output(outputs.ThreeDeeBrain())
+    pipeline.initialize_all_nodes()
     """
 
     def __init__(self):
-        self._input = None
-        self._frequency = None
-        self._processors = list()
-        self._outputs = list()
+        self._source = None  # type: SourceNode
+        self._processors = list()  # type: List[ProcessorNode]
+        self._outputs = list()  # type: List[OutputNode]
+        self._inputs_of_outputs = list()  # type: List[(SourceNode, ProcessorNode)]
 
     @property
-    def input(self):
-        return self._input
+    def source(self):
+        return self._source
 
-
-    @input.setter
+    @source.setter
     @accepts(object, SourceNode)
-    def input(self, input_node):
-        self._input = input_node
+    def source(self, input_node):
+        self._source = input_node
+        self._reconnect_outputs_to_last_node()  # In case some outputs were added before anything else
+
+    @property
+    def all_nodes(self) -> List[Node]:
+        return [self._source] + self._processors + self._outputs
+
+    @property
+    def frequency(self) -> (int, float):
+        try:
+            return self.source.frequency
+        except AttributeError:
+            raise ValueError("No source has been set in the pipeline")
+
 
     @accepts(object, ProcessorNode)
     def add_processor(self, processor_node):
         if processor_node not in self._processors:
+            last_node = self._last_node_before_outputs()
+            processor_node.input_node = processor_node.input_node or last_node
             self._processors.append(processor_node)
+            self._reconnect_outputs_to_last_node()
+        else:
+            msg = "Trying to add a {} that has already been added".format(class_name_of(processor_node))
+            raise ValueError(msg)
 
-    @accepts(object, OutputNode)
-    def add_output(self, output_node):
+    @accepts(object, OutputNode, (SourceNode, ProcessorNode))
+    def add_output(self, output_node, input_node=None):
+        """If input_node is None, output_node will be kept connected to whatever node that is currently last"""
         if output_node not in self._outputs:
             self._outputs.append(output_node)
+            # If input_node is None we will need to reconnect output_node. So we keep track of those Nones.
+            self._inputs_of_outputs.append(input_node)
+            output_node.input_node = input_node or self._last_node_before_outputs()
+        else:
+            msg = "Trying to add a {} that has already been added".format(class_name_of(output_node))
+            raise ValueError(msg)
 
-    def run(self):
-        all_nodes = [self._input] + self._processors + self._outputs
-        for node in all_nodes:
+    def _last_node_before_outputs(self):
+        try:
+            return self._processors[-1]
+        except IndexError:
+            return self.source
+
+    def initialize_all_nodes(self):
+        for node in self.all_nodes:
             node.initialize()
 
-        while self.input.is_alive: # TODO: also stop if all outputs are dead
-            for node in all_nodes:
+    def update_all_nodes(self):
+        for node in self.all_nodes:
+            node.update()
+
+    def run(self):
+        while self.source.is_alive:  # TODO: also stop if all outputs are dead
+            for node in self.all_nodes:
                 node.update()
+
+    def _reconnect_outputs_to_last_node(self):
+        """Reconnects all outputs that did not have an input node specified when added"""
+        last_node = self._last_node_before_outputs()
+        for output, input in zip(self._outputs, self._inputs_of_outputs):
+            output.input_node = input or last_node
+
+    def _reconnect_first_processor(self):
+        try:
+            self._processors[0].input_node = self.source
+        except IndexError:  # No processors have been added yet
+            pass
