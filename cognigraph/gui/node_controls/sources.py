7c24d30faf248abb40e226b96d95138d51e3b474
Evgenii <e.kalenkovich@gmail.com>
2017-11-24 08:35:51 +0300

    Updated for explicit setting of child attributes (see issue #8)

--- cognigraph/gui/node_controls/sources.py
+++ cognigraph/gui/node_controls/sources.py
@@ -6,23 +6,21 @@ from ...helpers.pyqtgraph import MyGroupParameter
 
 class LSLStreamSourceControls(MyGroupParameter):
     STREAM_NAME_PLACEHOLDER = 'Click here to choose a stream'
-    STREAM_NAMES_COMBO_NAME = 'stream names combo'
+    STREAM_NAMES_COMBO_NAME = 'Choose a stream: '
 
     def __init__(self, pipeline, **kwargs):
 
-        self._pipelne = pipeline
+        self._pipeline = pipeline
 
         kwargs['title'] = 'LSL stream settings'
         super().__init__(**kwargs, )
 
         stream_names = [info.name() for info in pylsl.resolve_streams()]
         values = [self.STREAM_NAME_PLACEHOLDER] + stream_names
-        stream_names_combo = parameterTypes.ListParameter(name=self.STREAM_NAMES_COMBO_NAME, title='Choose a stream: ',
+        stream_names_combo = parameterTypes.ListParameter(name=self.STREAM_NAMES_COMBO_NAME,
                                                           values=values, value=self.STREAM_NAME_PLACEHOLDER)
         stream_names_combo.sigValueChanged.connect(self._on_stream_name_picked)
-        stream_names_combo.sigDefaultChanged.connect(self._remove_placeholder_option)
-
-        self.addChild(stream_names_combo)
+        self.stream_names_combo = self.addChild(stream_names_combo)
 
     def _on_stream_name_picked(self, param, value):
         pass
@@ -30,5 +28,8 @@ class LSLStreamSourceControls(MyGroupParameter):
     def _remove_placeholder_option(self, default):
         stream_names_combo = self.param(self.STREAM_NAMES_COMBO_NAME)
         values = stream_names_combo.opts['values']  # type: list
-        values.remove(self.STREAM_NAME_PLACEHOLDER)
-        self.setLimits(values)
+        try:
+            values.remove(self.STREAM_NAME_PLACEHOLDER)
+            self.setLimits(values)
+        except ValueError:  # The placeholder option has already been removed
+            pass
