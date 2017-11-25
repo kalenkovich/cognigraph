e6dc3ddf9e8ee4a794c2f82e6843e4ebcbb588eb
Evgenii <e.kalenkovich@gmail.com>
2017-11-25 14:40:31 +0300

    Moved ExponentialMatrixSmoother out of pynfb to switch to master

--- cognigraph/helpers/pynfb.py
+++ cognigraph/helpers/pynfb.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.signal import lfilter
+
+from vendor.nfb.pynfb.signal_processing.filters import BaseFilter
 
 from .. import TIME_AXIS, PYNFB_TIME_AXIS
 
@@ -11,3 +14,18 @@ def pynfb_ndarray_function_wrapper(pynfb_function):
         else:
             return pynfb_function(ndarray.T).T
     return wrapped
+
+
+class ExponentialMatrixSmoother(BaseFilter):
+    def __init__(self, factor, column_count):
+        self.a = [1, -factor]
+        self.b = [1 - factor]
+        self.column_count = column_count
+        self.reset()
+
+    def apply(self, chunk: np.ndarray):
+        y, self.zi = lfilter(self.b, self.a, chunk, axis=0, zi=self.zi)
+        return y
+
+    def reset(self):
+        self.zi = np.zeros((max(len(self.a), len(self.b)) - 1, self.column_count))
