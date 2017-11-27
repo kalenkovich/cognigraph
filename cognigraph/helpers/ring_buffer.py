a8c2ff951c5fea5cce31822acc4889b1dd025812
Evgenii <e.kalenkovich@gmail.com>
2017-11-27 19:54:20 +0300

    Updated RingBuffer

--- cognigraph/helpers/ring_buffer.py
+++ cognigraph/helpers/ring_buffer.py
@@ -2,36 +2,55 @@ import numpy as np
 
 
 class RingBuffer(object):
+    """Represents a multi-row deque object"""
+    TIME_AXIS = 1
 
     def __init__(self, row_cnt, maxlen):
         self.maxlen = maxlen
         self.row_cnt = row_cnt
-        self._data = np.empty((row_cnt, maxlen))
-        self._current_sample_cnt = 0  # self._data[:, :self._filled_up_to] contains meaningful data
+        self._data = np.zeros((row_cnt, maxlen))
+        self._start = 0
+        self._current_sample_count = 0
 
     def extend(self, array):
-        self.check_shape(array)
+        self._check_input_shape(array)
+        new_sample_cnt = array.shape[self.TIME_AXIS]
 
-        new_sample_cnt = array.shape[1]
-        total_sample_cnt = self._current_sample_cnt + new_sample_cnt
-        samples_to_pop_cnt = max(total_sample_cnt - self.maxlen, 0)
-        samples_to_keep_cnt = self._current_sample_cnt - samples_to_pop_cnt
+        # If new data will take all the space, we can forget about the old data
+        if new_sample_cnt >= self.maxlen:
+            indices = np.arange(-self.maxlen, 0) + new_sample_cnt  # last self.maxlen samples
+            self._data = array.take(indices=indices, axis=1)
+            self._start = 0
+            self._current_sample_count = self.maxlen
 
-        self._current_sample_cnt = min(total_sample_cnt, self.maxlen)  # the number of samples after adding the new ones
+        else:
+            # New data should start after the end of the old one
+            new_data_start = (self._start + self._current_sample_count) % self.maxlen
 
-        self._data = np.roll(self._data, shift=-samples_to_pop_cnt, axis=1)  # pop samples that are too old to keep
-        # Write new samples. Every new sample that fits goes in
-        self._data[:, samples_to_keep_cnt:self._current_sample_cnt] = array[:, -self.maxlen:]
+            # Put as much as possible after new_data_start.
+            new_data_end = min(new_data_start + new_sample_cnt, self.maxlen)
+            self._data[:, new_data_start:new_data_end] = array[:, :(new_data_end-new_data_start)]
 
-    def check_shape(self, array):
+            # Then wrap around if needed
+            if new_data_end - new_data_start < new_sample_cnt:
+                new_data_end = (new_data_start + new_sample_cnt) % self.maxlen
+                self._data[:, :new_data_end] = array[:, -new_data_end:]
+
+            self._current_sample_count = min(self._current_sample_count + new_sample_cnt, self.maxlen)
+            if self._current_sample_count == self.maxlen:  # The buffer is fully populated
+                self._start = new_data_end % self.maxlen
+
+    def _check_input_shape(self, array):
         if array.shape[0] != self.row_cnt:
             msg = 'Wrong shape. You are trying to extend a buffer with {} rows with an array with {} rows'.format(
                 self.row_cnt, array.shape[0])
             raise ValueError(msg)
 
     def clear(self):
-        self._current_sample_cnt = 0
+        self._current_sample_count = 0
+        self._start = 0
 
     @property
     def data(self):
-        return self._data[:, :self._current_sample_cnt]
+        indices = self._start +  np.arange(self._current_sample_count)
+        return self._data.take(indices=indices, axis=self.TIME_AXIS, mode='wrap')
