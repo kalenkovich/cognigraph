0b5396a9373ee770b4d2a0a29fbc248c3a160117
Evgenii <e.kalenkovich@gmail.com>
2017-11-28 08:15:15 +0300

    Added support for generic EEG plus refactoring

---
+++ cognigraph/helpers/inverse_model.py
@@ -0,0 +1,97 @@
+import os
+
+import numpy as np
+import mne
+from mne.datasets import sample
+from mne.minimum_norm import read_inverse_operator
+
+data_path = sample.data_path(verbose='ERROR')
+neuromag_inverse_file_path = os.path.join(data_path, 'MEG', 'sample', 'sample_audvis-meg-oct-6-meg-inv.fif')
+standard_1005_forward_file_path = os.path.join(data_path, 'MEG', 'sample', 'sample_1005-eeg-oct-6-fwd.fif')
+standard_1005_inverse_file_path = os.path.join(data_path, 'MEG', 'sample', 'sample_1005-eeg-oct-6-eeg-inv.fif')
+
+
+def _fake_standard_1005_info(channel_labels):
+    montage_1005 = mne.channels.read_montage(kind='standard_1005')
+    montage_labels_upper = [label.upper() for label in montage_1005.ch_names]
+    ch_types = ['eeg' if label.upper() in montage_labels_upper else 'misc'
+                for label in channel_labels]
+    fake_info = mne.create_info(ch_names=channel_labels, sfreq=1000, ch_types=ch_types, montage=montage_1005)
+    return fake_info
+
+def _make_standard_1005_inverse_operator(channel_labels):
+
+    forward = mne.read_forward_solution(standard_1005_forward_file_path)
+    fake_info = _fake_standard_1005_info(channel_labels)
+    cov = mne.Covariance(data=np.identity(fake_info['nchan']),
+                         names=fake_info['ch_names'],
+                         bads=fake_info['bads'],
+                         projs=fake_info['projs'],
+                         nfree=1)
+
+    return mne.minimum_norm.make_inverse_operator(fake_info, forward, cov)
+
+
+def get_inverse_model_matrix_from_labels(channel_labels, snr, method):
+    montage_1005 = mne.channels.read_montage(kind='standard_1005')
+    montage_labels_upper = [montage_label.upper() for montage_label in montage_1005.ch_names]
+
+    if max(label.startswith('MEG ') for label in channel_labels) is True:
+        # sample.data_path() will also download 1.5 Gb so call it only in this branch
+
+        inverse_operator = read_inverse_operator(neuromag_inverse_file_path)
+        mne_inverse_model_file_path = neuromag_inverse_file_path
+        full_inverse_model_matrix = _matrix_from_inverse_operator(inverse_operator, snr, method)
+        inverse_model_matrix = _pick_channels_from_matrix(full_inverse_model_matrix, channel_labels,
+                                                          inverse_operator['info']['ch_names'])
+
+
+    elif any([channel_label.upper() in montage_labels_upper for channel_label in channel_labels]):
+
+        inverse_operator = _make_standard_1005_inverse_operator(channel_labels)
+        mne_inverse_model_file_path = standard_1005_inverse_file_path
+        inverse_model_matrix = _matrix_from_inverse_operator(inverse_operator, snr, method)
+
+    return inverse_model_matrix, mne_inverse_model_file_path
+
+
+def _pick_channels_from_matrix(full_inverse_model_matrix, channel_labels, inverse_operator_channel_labels):
+
+    # Let's pick an appropriate row for each label in channel_labels. Channels that are not in the inverse
+    # operator will get an all-zeros row.
+    inverse_operator_channel_count = len(inverse_operator_channel_labels)
+    picker_matrix = np.zeros((inverse_operator_channel_count, len(channel_labels)))
+    for (label_index, label) in enumerate(channel_labels):
+        try:
+            label_index_in_operator = inverse_operator_channel_labels.index(label)
+            picker_matrix[label_index_in_operator, label_index] = 1
+        except ValueError:
+            pass
+    return full_inverse_model_matrix.dot(picker_matrix)
+
+
+def _matrix_from_inverse_operator(inverse_operator, snr, method) -> np.ndarray:
+    # Create a dummy mne.Raw object
+    channel_count = inverse_operator['info']['nchan']
+    I = np.identity(channel_count)
+    dummy_info = inverse_operator['info']
+    dummy_info['sfreq'] = 1000
+    dummy_info['projs'] = list()
+    dummy_raw = mne.io.RawArray(data=I, info=inverse_operator['info'], verbose='ERROR')
+    contains_eeg_channels = len(mne.pick_types(dummy_info, meg=False, eeg=True)) > 0
+    if contains_eeg_channels:
+        dummy_raw.set_eeg_reference(verbose='ERROR')
+
+    # Applying inverse modelling to an identity matrix gives us the forward model matrix
+    lambda2 = 1.0 / snr ** 2
+    stc = mne.minimum_norm.apply_inverse_raw(dummy_raw, inverse_operator, lambda2, method)
+
+    return stc.data
+
+
+def get_inverse_model_matrix(mne_inverse_model_file_path, channel_labels, snr, method):
+    inverse_operator = read_inverse_operator(mne_inverse_model_file_path)
+    full_inverse_model_matrix = _matrix_from_inverse_operator(inverse_operator, snr, method)
+    inverse_model_matrix = _pick_channels_from_matrix(full_inverse_model_matrix, channel_labels,
+                                                      inverse_operator['info']['ch_names'])
+    return inverse_model_matrix
--- cognigraph/nodes/processors.py
+++ cognigraph/nodes/processors.py
@@ -1,12 +1,10 @@
-import os
+from typing import Tuple
 
 import numpy as np
-import mne
-from mne.datasets import sample
-from mne.minimum_norm import read_inverse_operator
 
 from .node import ProcessorNode
 from ..helpers.matrix_functions import make_time_dimension_second, put_time_dimension_back_from_second
+from ..helpers.inverse_model import get_inverse_model_matrix, get_inverse_model_matrix_from_labels
 from ..helpers.pynfb import pynfb_ndarray_function_wrapper, ExponentialMatrixSmoother
 from vendor.nfb.pynfb.signal_processing import filters
 
@@ -51,53 +49,15 @@ class InverseModel(ProcessorNode):
 
     def _initialize(self):
         channel_labels = self.traverse_back_and_find('channel_labels')
-        self._inverse_model_matrix = self._assemble_inverse_model_matrix(channel_labels)
+        if self.mne_inverse_model_file_path is None:
+            self._inverse_model_matrix, self.mne_inverse_model_file_path = \
+                get_inverse_model_matrix_from_labels(channel_labels, snr=self.snr, method=self.method)
+        else:
+            self._inverse_model_matrix = get_inverse_model_matrix(self.mne_inverse_model_file_path, channel_labels,
+                                                                  snr=self.snr, method=self.method)
+
         self.channel_count = self._inverse_model_matrix.shape[0]
         self.channel_labels = ['vertex #{}'.format(i + 1) for i in range(self.channel_count)]
-
-    def _assemble_inverse_model_matrix(self, channel_labels):
-        # Either use the user-provided inverse model file or choose one of the default ones based on channel labels.
-        if self.mne_inverse_model_file_path is None:
-            self.mne_inverse_model_file_path = self._pick_inverse_model_based_on_channel_labels(channel_labels)
-        inverse_operator = read_inverse_operator(self.mne_inverse_model_file_path, verbose='ERROR')
-
-        # First, let's extract the inverse model matrix for all the channels in the inverse operator
-        full_operator_matrix = self._matrix_from_inverse_operator(inverse_operator, snr=self.snr, method=self.method)
-
-        # Now let's pick an appropriate row for each label in channel_labels. Channels that are not in the inverse
-        # operator will get an all-zeros row.
-        inverse_operator_channel_count = inverse_operator['info']['nchan']
-        inverse_operator_channel_labels = inverse_operator['info']['ch_names']
-        picker_matrix = np.zeros((inverse_operator_channel_count, len(channel_labels)))
-        for (label_index, label) in enumerate(channel_labels):
-            try:
-                label_index_in_operator = inverse_operator_channel_labels.index(label)
-                picker_matrix[label_index_in_operator, label_index] = 1
-            except ValueError:
-                pass
-        return full_operator_matrix.dot(picker_matrix)
-
-    def _matrix_from_inverse_operator(self, inverse_operator, snr, method) -> np.ndarray:
-        # Create a dummy mne.Raw object
-        channel_count = inverse_operator['info']['nchan']
-        I = np.identity(channel_count)
-        dummy_info = inverse_operator['info']
-        dummy_info['sfreq'] = self.traverse_back_and_find('frequency')
-        dummy_info['projs'] = list()
-        dummy_raw = mne.io.RawArray(data=I, info=inverse_operator['info'], verbose='ERROR')
-        # dummy_raw.set_eeg_reference(verbose='ERROR')
-
-        # Applying inverse modelling to an identity matrix gives us the forward model matrix
-        lambda2 = 1.0 / snr ** 2
-        stc = mne.minimum_norm.apply_inverse_raw(dummy_raw, inverse_operator, lambda2, method)
-        return stc.data
-
-    def _pick_inverse_model_based_on_channel_labels(self, channel_labels):
-        if max(label.startswith('MEG ') for label in channel_labels):
-            # sample.data_path() will also download 1.5 Gb so call it only in this branch
-            neuromag_inverse = os.path.join(sample.data_path(),
-                                            'MEG', 'sample', 'sample_audvis-meg-oct-6-meg-inv.fif')
-            return neuromag_inverse
 
 
 class LinearFilter(ProcessorNode):
@@ -196,6 +156,20 @@ def pynfb_filter_based_processor_class(pynfb_filter_class):
     In this case LinearFilter should provide fs and n_channels parameters to filters.ButterFilter automatically
     """
     class PynfbFilterBasedProcessorClass(ProcessorNode):
+        def _check_value(self, key, value):
+            pass
+
+        @property
+        def CHANGES_IN_THESE_REQUIRE_RESET(self) -> Tuple[str]:
+            pass
+
+        @property
+        def UPSTREAM_CHANGES_IN_THESE_REQUIRE_REINITIALIZATION(self) -> Tuple[str]:
+            pass
+
+        def reset(self):
+            pass
+
         def __init__(self):
             pass
 
