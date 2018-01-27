1146e6762acef31dc1735394b9928510715b97cd
Evgenii <e.kalenkovich@gmail.com>
2018-01-27 14:15:05 +0300

    Finished interpolation matrix calculation

--- cognigraph/helpers/channels.py
+++ cognigraph/helpers/channels.py
@@ -1,19 +1,54 @@
-import os.path as op
-
 import mne
-from mne import io
-from mne.datasets import sample
+from mne.io.meas_info import _kind_dict
+import numpy as np
+
+from .. import MISC_CHANNEL_TYPE
 
 
-def calculate_interpolation_matrix(channel_labels, bad_channel_flags):
-    fake_raw = None;
-    fake_raw
-    return
+def calculate_interpolation_matrix(info: mne.Info):
+    """
+    Calculates a matrix that interpolates bad channels
+    :param info: mne.Info dict with names of channels to be interpolated under the 'bads' key
+    :return: matrix M such that M.dot(data) interpolates bad channels (assuming rows in data correspond to channels)
+    """
+    fill_eeg_channel_locations(info)
+    channel_count = info['nchan']
+    dummy_raw = mne.io.RawArray(data=np.identity(channel_count), info=info, verbose='ERROR')
+    dummy_raw.interpolate_bads(mode='fast')
+
+    return dummy_raw.get_data()
 
 
+def fill_eeg_channel_locations(info: mne.Info):
+    """
+    Add standard locations for EEG channels if none were provided. Operates in place.
+    """
 
-data_path = sample.data_path(verbose='ERROR')
-sample_raw_path = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw.fif')
-raw = io.read_raw_fif(sample_raw_path)  # type: mne.io.Raw
-raw.load_data();
-raw2 = raw.interpolate_bads(reset_bads=False, mode='fast')
+    # To interpolate we need all EEG channels to have locations, including bad channels
+    eeg_channel_ids = mne.pick_types(info, eeg=True, exclude=[])
+
+    # EEG electrode locations are stored in the first three elements of the 'loc' list in info['chs'][idx].
+    # If no location is set, then all of them will be zero.
+    channels_wo_location = [info['chs'][eeg_channel_id] for eeg_channel_id in eeg_channel_ids
+                            if np.all(info['chs'][eeg_channel_id]['loc'][:3] == [0, 0, 0])]
+
+    if any(channels_wo_location):
+        print('{channel_count} EEG channels have no locations assigned. Default values will be used.'.format(
+            channel_count=len(channels_wo_location)
+        ))
+        eeg_montage = mne.channels.read_montage(kind='standard_1005')  # type: mne.channels.Montage
+        for channel in channels_wo_location:
+            try:
+                channel_idx_in_montage = eeg_montage.ch_names.index(channel['ch_name'])
+            except ValueError:
+                ch_name = channel['ch_name']
+                print('Could not find channel {name} in the standard montage. Will set it as {misc}'.format(
+                    name=ch_name, misc=MISC_CHANNEL_TYPE))
+                _set_channel_as_misc(channel)
+            else:
+                channel['loc'][:3] = eeg_montage.pos[channel_idx_in_montage]
+
+
+def _set_channel_as_misc(channel):
+    kind = _kind_dict[MISC_CHANNEL_TYPE][0]
+    channel['kind'] = kind
