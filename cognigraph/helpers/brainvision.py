88d256485491f767065e8586a9a8da7d7829432b
Evgenii <e.kalenkovich@gmail.com>
2018-02-09 12:24:06 +0300

    Added option to only load a certain time interval
    Also added conforming to time_axis parameter

--- cognigraph/helpers/brainvision.py
+++ cognigraph/helpers/brainvision.py
@@ -5,6 +5,8 @@ import mne.io.brainvision.brainvision as brainvision
 brainvision._check_mrk_version_original = brainvision._check_mrk_version
 brainvision._check_hdr_version_original = brainvision._check_hdr_version
 
+BRAINVISION_TIME_AXIS = 1
+
 def _check_mrk_version(header):
     bobe_mrk_header = 'BrainVision Data Exchange Marker File, Version 1.0'
     if header == bobe_mrk_header:
@@ -25,8 +27,17 @@ brainvision._check_mrk_version = _check_mrk_version
 # End of accommodating spelling mistakes
 
 
-def read_brain_vision_data(vhdr_file_path, time_axis):
-    raw = mne.io.read_raw_brainvision(vhdr_fname=vhdr_file_path, verbose='ERROR')
-    data = raw.get_data()
+def read_brain_vision_data(vhdr_file_path, time_axis, start_s: int=0, stop_s: int=None):
+    raw = mne.io.read_raw_brainvision(vhdr_fname=vhdr_file_path, verbose='ERROR')  # type: mne.io.Raw
+
+    # Get the required time slice. mne.io.Raw.get_data takes array indices, not time
+    start = 0 if start_s is None else raw.time_as_index(start_s)[0]
+    stop = min(raw.n_times if stop_s is None else raw.time_as_index(stop_s), raw.n_times)
+    data = raw.get_data(start=start, stop=stop)
+
     mne_info = raw.info.copy()
+
+    if time_axis == BRAINVISION_TIME_AXIS:
+        data = data.T
+
     return data, mne_info
