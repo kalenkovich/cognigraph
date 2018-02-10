a49157e558eb464aad79bcd4df2a6c3bb4bc17b9
Evgenii <e.kalenkovich@gmail.com>
2018-02-10 15:33:53 +0300

    Monkey-patching mne-python's brainvision module only once
    Before, it would break on reload

--- cognigraph/helpers/brainvision.py
+++ cognigraph/helpers/brainvision.py
@@ -1,30 +1,35 @@
 import mne
 import mne.io.brainvision.brainvision as brainvision
 
-# Start of accommodating spelling mistakes
-brainvision._check_mrk_version_original = brainvision._check_mrk_version
-brainvision._check_hdr_version_original = brainvision._check_hdr_version
 
 BRAINVISION_TIME_AXIS = 1
 
-def _check_mrk_version(header):
-    bobe_mrk_header = 'BrainVision Data Exchange Marker File, Version 1.0'
-    if header == bobe_mrk_header:
-        return True
-    else:
-        return brainvision._check_mrk_version_original(header)
+
+# Monkey-patch to accommodate for spelling mistakes in the header file
+if not hasattr(brainvision, '_check_version_monkey_patched'):  # Do not repeat upon reimporting
+    def _check_mrk_version(header):
+        bobe_mrk_header = 'BrainVision Data Exchange Marker File, Version 1.0'
+        if header == bobe_mrk_header:
+            return True
+        else:
+            return brainvision._check_mrk_version_original(header)
 
 
-def _check_hdr_version(header):
-    bobe_hdr_header = 'BrainVision Data Exchange Header File Version 1.0'
-    if header == bobe_hdr_header:
-        return True
-    else:
-        return brainvision._check_hdr_version_original(header)
+    def _check_hdr_version(header):
+        bobe_hdr_header = 'BrainVision Data Exchange Header File Version 1.0'
+        if header == bobe_hdr_header:
+            return True
+        else:
+            return brainvision._check_hdr_version_original(header)
 
-brainvision._check_hdr_version = _check_hdr_version
-brainvision._check_mrk_version = _check_mrk_version
-# End of accommodating spelling mistakes
+
+    brainvision._check_hdr_version_original = brainvision._check_hdr_version
+    brainvision._check_hdr_version = _check_hdr_version
+
+    brainvision._check_mrk_version_original = brainvision._check_mrk_version
+    brainvision._check_mrk_version = _check_mrk_version
+
+    brainvision._check_version_monkey_patched = True
 
 
 def read_brain_vision_data(vhdr_file_path, time_axis, start_s: int=0, stop_s: int=None):
